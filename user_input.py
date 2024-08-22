9fa89a5ed1285402ee8a1a40cc59c0ce1fe67b8c
Charles Wakaba <charleswakaba@gmail.com>
2024-08-22 12:02:03 +0300

    Week 1 python assignment

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name = input("what is your name:")
+age = input("what is your age:")
+location = input("where do you live:")
+
+print(name)
+print(age)
+print(location)
+
+print (f"Hello my name is {name} plus I am this old {age} and I live in {location}")
